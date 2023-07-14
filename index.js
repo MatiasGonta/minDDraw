f31a53b09ca5058f37b40208844d841eef18f07c
Matías Gonta <maret2023@gmail.com>
2023-07-14 18:28:43 -0300

    complete refactoring

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const canvas = document.querySelector("canvas");
 
 const ctx = canvas.getContext("2d");
 
-let mouseX, mouseY, canvasShot, isDrawing = false, tool = "brush", color = "#000000", lineWidth = 5;
+let mouseX, mouseY, canvasShot, previousTool, isDrawing = false, tool = "brush", color = "#000000", lineWidth = 5;
 
 const colorPicker = document.getElementById("color-picker");
+
+//History Colors
 const firstHistoryColor = document.querySelector(".color:nth-child(2)");
 const secondHistoryColor = document.querySelector(".color:nth-child(3)");
 const thirdHistoryColor = document.querySelector(".color:nth-child(4)");
 const fourthHistoryColor = document.querySelector(".color:nth-child(5)");
-firstHistoryColor.id = "rgb(255, 0, 0)";
-secondHistoryColor.id = "rgb(0, 0, 255)";
-thirdHistoryColor.id = "rgb(0, 255, 0)";
-fourthHistoryColor.id = "rgb(255, 255, 0)";
-let historyColor = [firstHistoryColor.id,secondHistoryColor.id,thirdHistoryColor.id,fourthHistoryColor.id];
+const fifthHistoryColor = document.querySelector(".color:nth-child(6)");
+firstHistoryColor.id = 'rgb(0, 0, 0)';
+secondHistoryColor.id = 'rgb(255, 0, 0)';
+thirdHistoryColor.id = 'rgb(0, 0, 255)';
+fourthHistoryColor.id = 'rgb(0, 255, 0)';
+fifthHistoryColor.id = 'rgb(255, 255, 0)';
+
+let historyColor = [
+    firstHistoryColor.id,
+    secondHistoryColor.id,
+    thirdHistoryColor.id,
+    fourthHistoryColor.id,
+    fifthHistoryColor.id
+];
 
 const toolButtons = document.querySelectorAll(".tool");
 const colorButtons = document.querySelectorAll(".color");
@@ -23,9 +34,6 @@ const colorButtons = document.querySelectorAll(".color");
 const rangeDensity = document.getElementById("range-density");
 
 const fillColor = document.getElementById("fill-check");
-
-const clearCanvas = document.getElementById("clear-canvas");
-const saveCanvas = document.getElementById("save-canvas");
 
 const setCanvasBackground = () => {
     ctx.fillStyle = "#fff";
@@ -72,22 +80,44 @@ const drawing = (e) => {
         return;
     }
     ctx.putImageData(canvasShot, 0, 0);
-    if(tool === "brush" || tool === "eraser") {
-        ctx.strokeStyle = tool === "eraser" ? "#fff" : color;
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-    } else if(tool === "rectangle"){
-        drawRectangle(e);
-    } else if(tool === "circle"){
-        drawCircle(e);
-    } else {
-        drawTriangle(e);
-    }
+
+    const drawingFunctions = {
+        brush: () => {
+          ctx.strokeStyle = color;
+          ctx.lineTo(e.offsetX, e.offsetY);
+          ctx.stroke();
+        },
+        eraser: () => {
+          ctx.strokeStyle = "#fff";
+          ctx.lineTo(e.offsetX, e.offsetY);
+          ctx.stroke();
+        },
+        rectangle: () => drawRectangle(e),
+        circle: () => drawCircle(e),
+        triangle: () => drawTriangle(e)
+    };
+
+    const drawingFunction = drawingFunctions[tool];
+    drawingFunction();
 }
 
+//Canvas Listeners
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", ()=> {
+    isDrawing = false;
+});
+
+//Handle Tools, Color Buttons, Draw Density input, Color Picker input and History Colors
 toolButtons.forEach(button => {
     button.addEventListener("click", () => {
         tool = button.id;
+
+        if (previousTool) {
+            previousTool.style.color = '#FF6B6B';
+        }
+        button.style.color = '#FFCAAF';
+        previousTool = button;
     });
 });
 
@@ -110,12 +140,19 @@ colorPicker.addEventListener("change", ()=> {
     secondHistoryColor.style.backgroundColor = historyColor[1];
     thirdHistoryColor.style.backgroundColor = historyColor[2];
     fourthHistoryColor.style.backgroundColor = historyColor[3];
+    fifthHistoryColor.style.backgroundColor = historyColor[4];
 
     firstHistoryColor.id = historyColor[0];
     secondHistoryColor.id = historyColor[1];
     thirdHistoryColor.id = historyColor[2];
     fourthHistoryColor.id = historyColor[3];
+    fifthHistoryColor.id = historyColor[4];
 });
+
+//Functions Buttons
+const clearCanvas = document.getElementById("clear-canvas");
+const saveCanvas = document.getElementById("save-canvas");
+const link = document.getElementById("download-link");
 
 clearCanvas.addEventListener("click", ()=> {
     ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -123,23 +160,19 @@ clearCanvas.addEventListener("click", ()=> {
 });
 
 saveCanvas.addEventListener("click", ()=> {
-    const link = document.createElement("a");
     link.download = `${Date.now()}.jpg`;
     link.href = canvas.toDataURL();
-    link.click();
 });
 
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", ()=> {
-    isDrawing = false;
-});
+// Menu Bar
+const toolsBoard = document.querySelector(".tools-board");
+const lines = document.querySelectorAll(".line");
+const menuBar = document.querySelector(".menu-bar");
+menuBar.addEventListener("click", ()=> {
+    lines[0].classList.toggle("activeLine1");
+    lines[1].classList.toggle("activeLine2");
+    lines[2].classList.toggle("activeLine3");
 
-//Social Networks popup
-const mentionName = document.getElementById("name").addEventListener("click", ()=> {
-    document.querySelector(".popup").classList.add("active");
-})
-
-const closeButton = document.querySelector(".close").addEventListener("click", ()=> {
-    document.querySelector(".popup").classList.remove("active");
+    menuBar.classList.toggle("mb-active");
+    toolsBoard.classList.toggle("nb-active")
 });
---
+++ readme.md
@@ -0,0 +1,79 @@
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/2560px-Flag_of_the_United_Kingdom.svg.png" alt="English" width="100px" />
+</p>
+
+---
+
+# minDDraw
+
+minDDraw is a drawing website similar to Microsoft Paint, which allows users to unleash their creativity and create digital works of art. The project provides a user-friendly interface with a toolbar at the top that includes various drawing tools like shapes, colors, and more. It has a canvas where users can draw, and at the bottom there are two buttons: one to completely clear the canvas and one to download the created drawing.
+
+![minDDraw-screenshot!](./assets/minddraw-screenshot.pngscreenshot1.png) ![minDDraw-video!](./assets/minddraw-video.mp4)
+
+## Features
+
+- Toolbar: The application includes a toolbar at the top, offering various drawing tools such as shapes, colors, and more.
+- Canvas where users can create their drawings.
+- Clear Canvas: There is a button available to clear the entire canvas and start a new drawing.
+- Download Drawing: Users can download their completed drawing as an image file, allowing them to save and share their artwork.
+- User-Friendly Interface: The application has an intuitive and user-friendly interface, making it easy for users to navigate and use the drawing tools.
+
+## Technologies Used
+
+- <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">**HTML**</a>: Markup language for creating web pages.
+- <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">**CSS**</a>: Styling language for designing the appearance of web pages.
+- <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">**JavaScript**</a>: Programming language for adding interactivity and dynamic features to web pages.
+
+## Project Structure
+
+- `assets`: This directory contains any necessary assets such as images or icons used in the project.
+- `index.html`: The main HTML file that serves as the entry point of the application.
+- `styles.css`: The CSS file that contains the styles for the web page.
+- `index.js`: The JavaScript file that handles the logic and functionality of the web page.
+
+## Authors
+
+- Matías Gonta
+
+If you have any questions, concerns or interests, feel free to contact me.
+
+---
+
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2560px-Flag_of_Spain.svg.png" alt="Spanish" width="100px" />
+</p>
+
+---
+
+# minDDraw
+
+minDDraw es un sitio web de dibujo similar a Microsoft Paint, que permite a los usuarios dar rienda suelta a su creatividad y crear obras de arte digitales. El proyecto proporciona una interfaz fácil de usar con una barra de herramientas en la parte superior que incluye varias herramientas de dibujo como formas, colores y más. Tiene un lienzo donde los usuarios pueden dibujar, y en la parte inferior hay dos botones: uno para borrar completamente el lienzo y otro para descargar el dibujo creado.
+
+![minDDraw-screenshot!](./assets/minddraw-screenshot.pngscreenshot1.png) ![minDDraw-video!](./assets/minddraw-video.mp4)
+
+## Características
+
+- Barra de herramientas: la aplicación incluye una barra de herramientas en la parte superior que ofrece varias herramientas de dibujo, como formas, colores y más.
+- Lienzo donde los usuarios pueden crear sus dibujos.
+- Borrar lienzo: hay un botón disponible para borrar todo el lienzo y comenzar un nuevo dibujo.
+- Descargar dibujo: los usuarios pueden descargar su dibujo completo como un archivo de imagen, lo que les permite guardar y compartir su obra de arte.
+- Interfaz fácil de usar: la aplicación tiene una interfaz intuitiva y fácil de usar, lo que facilita a los usuarios la navegación y el uso de las herramientas de dibujo.
+
+## Tecnologías utilizadas
+
+- <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">**HTML**</a>: Lenguaje de marcado para la creación de páginas web.
+- <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">**CSS**</a>: Lenguaje de estilo para diseñar la apariencia de paginas web.
+- <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">**JavaScript**</a>: Lenguaje de programación para agregar interactividad y características dinámicas a las páginas web.
+
+## Estructura del proyecto
+
+- `assets`: Este directorio contiene todos los activos necesarios, como imágenes o iconos utilizados en el proyecto.
+- `index.html`: El archivo HTML principal que sirve como punto de entrada de la aplicación.
+- `styles.css`: El archivo CSS que contiene los estilos para la página web.
+- `index.js`: El archivo JavaScript que maneja la lógica y la funcionalidad de la página web.
+
+## Autores
+
+-Matias Gonta
+
+Si tiene alguna pregunta, inquietud o interes, no dude en ponerse en contacto conmigo.
